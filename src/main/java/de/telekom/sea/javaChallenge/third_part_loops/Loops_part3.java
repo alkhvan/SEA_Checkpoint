5b64a88b1d5cf53e8b09a152c2348ce3cfb62cdb
Aleksandra Khvan <aleksandra.klochko@t-systems.com>
2021-06-02 11:43:02 +0300

    1 st task as text was added 3 rd task was added in the separate package

--- src/main/java/de/telekom/sea/javaChallenge/third_part_loops/Loops_part3.java
+++ src/main/java/de/telekom/sea/javaChallenge/third_part_loops/Loops_part3.java
@@ -1,15 +1,16 @@
 package de.telekom.sea.javaChallenge.third_part_loops;
 
 public class Loops_part3 {
-
-    public static void main (String[] args) {
+      public static void main (String[] args) {
         System.out.println("first loop");
-     //   loop1(i);
+        loop1();
         System.out.println("second loop");
+        loop2();
+
 
     }
-    public void loop1 (int i){
-        i = 0;
+    public static void loop1(){
+         int i = 0;
         //1st loop
         while (i < 8) {
             System.out.println(i);
@@ -17,8 +18,8 @@ public class Loops_part3 {
 
         }
     }
-    public void loop2 (int i){
-        i = 0;
+    public static void loop2 (){
+        int i = 0;
         //2nd loop
         do {System.out.println(i);
             i++;}
